2b60f8df7c67cfd15110569c1505c1cee7632141
konrad.rekawek <konrad.rekawek@aat.pl>
2020-05-22 12:48:57 +0200

    add ShowResultService

---
+++ ProcesProdukcji/ProcesProdukcji/Services/Implemenatations/ShowResultService.cs
@@ -0,0 +1,17 @@
+﻿using ProcesProdukcji.ModelsDTO;
+using ProcesProdukcji.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProcesProdukcji.Services.Implemenatations
+{
+    public class ShowResultService : IShowResultService
+    {
+        public ResultCostDTO PresentResult(string cityName, ModuleListDTO moduleListDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
